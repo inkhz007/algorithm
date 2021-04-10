763403ef5f2b1f2c24db9f49bd92a95409e90f17
ink <ink_hz@163.com>
2021-04-10 10:30:35 +0800

    [leetcode]206. Reverse Linked List

---
+++ leetcode/linked_list/206.Reverse_Linked_List.cpp
@@ -0,0 +1,112 @@
+/*
+    https://leetcode-cn.com/problems/reverse-linked-list/
+*/
+
+/*
+    递归反转单链表
+    1. 递归终止条件： head->next == NULL; 最后一个节点
+    2. 递归公式：reverse(head->next); head与一条已经反转好的节点相连
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ ListNode* reverse(ListNode* head) {
+     if (head->next == NULL) return head;
+     
+     ListNode* last = reverse(head->next);
+     head->next->next = head;
+     head->next = NULL;
+     return last;
+ }
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next ==NULL) {
+            return head;
+        }
+
+        return reverse(head);
+    }
+};   
+
+
+/*
+    迭代反转单链表
+    1. 遍历链表，头插法反转链表
+    2. head->next = NULL; 链表头节点作为反转后的尾节点，next指针需要置NULL
+    3. ListNode* tmp = cur->next; 因为要使用头插法，当前节点的next要指向new_head，所以需要先保存，防止丢失
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        ListNode* cur = head->next;
+        ListNode* rev_head = head;
+        head->next = NULL;
+        while(cur) {
+            ListNode* tmp = cur->next;
+            cur->next = rev_head;
+            rev_head = cur;
+            cur = tmp;
+        }
+
+        return rev_head;
+    }
+};
+
+
+/*
+  参照https://leetcode.com/problems/reverse-linked-list/discuss/58125/In-place-iterative-and-recursive-Java-solution优化代码
+  1. 不需要cur指针，直接使用head进行遍历
+  2. 不需要head->next 置为NULL，直接将NewHead置NULL开始
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* NewHead = NULL;
+        while(head) {
+            ListNode* next = head->next;
+            head->next = NewHead;
+            NewHead = head;
+            head = next;
+        }
+
+        return NewHead;
+    }
+};
+
---
+++ list/list.h
@@ -0,0 +1,6 @@
+/* singley-linked list*/
+struct ListNode {
+    int val;
+    struct ListNode* next;
+    //struct ListNode(int x) : val(0), next(NULL) {} 
+};
